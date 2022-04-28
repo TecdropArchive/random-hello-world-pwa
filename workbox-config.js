ede04c2262d8645e363c1e907a9d78b24c876b11
devliber <dev@devliber.com>
2022-04-28 17:10:44 +0300

    First commit (existing source code)

---
+++ workbox-config.js
@@ -0,0 +1,14 @@
+/* global module */
+
+module.exports = {
+    "globDirectory": "./docs",
+    "globPatterns": [
+        "*.html",
+        "scripts/*.js",
+        "data/*.json",
+        "styles/*.css",
+        "images/icons/*.svg"
+    ],
+    "swDest": "./docs/sw.js"
+};
+
